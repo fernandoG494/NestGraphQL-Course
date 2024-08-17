a2cb1e195f2253de2f79484f72a046649a157611
Fernando García (Incuerd0) <42920098+fernandoG494@users.noreply.github.com>
2024-08-17 13:28:58 -0600

    Nuevas funciones

--- 02-todo/src/hello-world/hello-world.resolver.ts
+++ 02-todo/src/hello-world/hello-world.resolver.ts
@@ -1,9 +1,22 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Float, Int, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class HelloWorldResolver {
-  @Query(() => String)
+  @Query(() => String, {
+    description: "Return 'hello world' string",
+    name: 'hello',
+  })
   helloWorld(): string {
     return 'Hello world';
+  }
+
+  @Query(() => Float, { name: 'randomNumber' })
+  getRandomNumber(): number {
+    return Math.random() * 100;
+  }
+
+  @Query(() => Int, { name: 'randomFromZeroTo' })
+  getRandomFromZeroTo(): number {
+    return Math.floor(Math.random() * 10);
   }
 }
