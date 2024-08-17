37f2332622147a9a5fcbe39e9cd4cd660681fe6a
Fernando García (Incuerd0) <42920098+fernandoG494@users.noreply.github.com>
2024-08-17 13:08:24 -0600

    hello world

--- 02-todo/src/app.module.ts
+++ 02-todo/src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { HelloWorldModule } from './hello-world/hello-world.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    HelloWorldModule,
+  ],
   controllers: [],
   providers: [],
 })
